50fc7c6c01e3d633a56133c25df1d9065a40409a
Ben Baker <bakerb845@gmail.com>
2022-01-12 08:50:32 -0700

    making required QT_VERSION > QT_VERSION_CHECK(5, 0, 0) should be replace with QT_VERSION >= QT_VERSION_CHECK(6, 0, 0) change

--- demo/samples/items.cpp
+++ demo/samples/items.cpp
@@ -17,7 +17,7 @@
  ****************************************************************************/
 #include "items.h"
 #include "ellipse.h"
-#if QT_VERSION > QT_VERSION_CHECK(5, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 #include <random>
 #endif
 
