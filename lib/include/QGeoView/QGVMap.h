49bcf6c349ecaff4c584ef7a05820170279df24e
Transporter <ogre.transporter@gmail.com>
2024-05-19 14:49:56 +0200

    Add mapMouseDoubleClicked signal and mouseDoubleClickEvent override

--- lib/include/QGeoView/QGVMap.h
+++ lib/include/QGeoView/QGVMap.h
@@ -90,6 +90,7 @@ public:
 protected:
     void mouseMoveEvent(QMouseEvent* event) override;
     void mousePressEvent(QMouseEvent* event) override;
+    void mouseDoubleClickEvent(QMouseEvent* event) override;
 
 Q_SIGNALS:
     void projectionChanged();
@@ -102,6 +103,7 @@ Q_SIGNALS:
     void itemDoubleClicked(QGVItem* item, QPointF projPos);
     void mapMouseMove(QPointF projPos);
     void mapMousePress(QPointF projPos);
+    void mapMouseDoubleClicked(QPointF projPos);
 
 private:
     QScopedPointer<QGVProjection> mProjection;
